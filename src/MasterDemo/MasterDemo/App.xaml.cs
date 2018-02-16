0a4ac6394ffd5b95a6061a5ec2fe475c7640f019
blitzard7 <samanta-07@hotmail.com>
2018-02-16 11:28:49 +0100

    MasterPage, DetailPage

--- src/MasterDemo/MasterDemo/App.xaml.cs
+++ src/MasterDemo/MasterDemo/App.xaml.cs
@@ -11,17 +11,17 @@ namespace MasterDemo
 			MainPage = new MasterDemo.MainPage();
 		}
 
-		protected override void OnStart ()
+		protected override void OnStart()
 		{
 			// Handle when your app starts
 		}
 
-		protected override void OnSleep ()
+		protected override void OnSleep()
 		{
 			// Handle when your app sleeps
 		}
 
-		protected override void OnResume ()
+		protected override void OnResume()
 		{
 			// Handle when your app resumes
 		}
